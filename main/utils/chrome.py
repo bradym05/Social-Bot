6133ca5ab1a13e2be5aa1e2b67a9303f14c58508
Brady Maki <78286012+bradym05@users.noreply.github.com>
2026-02-28 15:37:18 -0700

    Detection patches
    Window properties starting with "cdc_" are removed to prevent chromedriver being detected (https://github.com/dobiadi/bot-detect/blob/master/src/collector/detections/chromedriver/README.md).
    
    Still need to bypass error trapping somehow.
    
    Ran tests using: https://github.com/dobiadi/bot-detect
    https://deviceandbrowserinfo.com/are_you_a_bot
    https://deviceandbrowserinfo.com/are_you_a_bot_interactions

--- main/utils/chrome.py
+++ main/utils/chrome.py
@@ -1,10 +1,25 @@
 from selenium import webdriver
 
+def remove_cdp_props(driver):
+    """
+    Removes CDP props from a webdriver instance, permanently
+    - Implementation from: https://github.com/ultrafunkamsterdam/undetected-chromedriver/issues/986
+    """
+    # Get list of props
+    cdc_props = driver.execute_script('const j=[];for(const p in window){'
+                                    'if(/^[a-z]{3}_[a-zA-Z0-9]{22}_.*/i.test(p)){'
+                                    'j.push(p);delete window[p];}}return j;')
+    # Check if any cdc props are presnt
+    if len(cdc_props) > 0:
+        cdc_props_js_array = '[' + ','.join('"' + p + '"' for p in cdc_props) + ']'
+        driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',
+                            {'source': cdc_props_js_array + '.forEach(k=>delete window[k]);'})
+
 def no_indicators():
     """
     Returns a webdriver instance with:
-    - Fake chrome user agent
     - Automation indicators removed (https://www.zenrows.com/blog/selenium-avoid-bot-detection#disable-automation-indicator-webdriver-flags)
+    - CDP properties removed (https://github.com/dobiadi/bot-detect/blob/master/src/collector/detections/chromedriver/README.md)
     """
     # Setup options
     options = webdriver.ChromeOptions()
@@ -14,4 +29,6 @@ def no_indicators():
     # Create and return driver
     driver = webdriver.Chrome(options=options)
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
+    # Custom scripts
+    remove_cdp_props(driver)
     return driver
